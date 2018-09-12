9b3eb07e58b4547bd87cf14a7b39917a3180cdc2
Dominik <36657497+D0miH@users.noreply.github.com>
2018-09-12 10:05:43 +0200

    basic layout with textbox and send button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React from "react";
 import Sidebar from "./components/Sidebar/Sidebar";
 import TextField from "./components/TextEntry/TextField";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#3E424C",
+            light: "#494D59",
+            dark: "#292C33",
+            contrastText: "#B0BBD9"
+        }
+    }
+});
 
 class App extends React.Component {
-    render() {
-        console.log(window.screen);
-        return (
-            <div className="App" style={{ backgroundColor: "#3E424C", width: "100%", height: "100%" }}>
-                <Sidebar />
+    state = {
+        windowWidth: window.innerWidth
+    };
 
-                <div className={"AppContent"} style={{ position: "relative", float: "left", width: window.innerWidth - 251, height: "100%" }}>
-                    <TextField />
+    componentDidMount() {
+        window.addEventListener("resize", (e) => this.handleResize(e));
+        this.setState({ windowWidth: window.innerWidth });
+    }
+
+    handleResize(e) {
+        e.preventDefault();
+        this.setState({ windowWidth: window.innerWidth });
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider theme={theme}>
+                <div className="App" style={{ backgroundColor: "#3E424C", width: "100%", height: "100%" }}>
+                    <Sidebar />
+
+                    <div
+                        className={"AppContent"}
+                        style={{ position: "relative", float: "left", width: this.state.windowWidth - 251, height: "100%" }}
+                    >
+                        <TextField width={this.state.windowWidth - 251} />
+                    </div>
                 </div>
-            </div>
+            </MuiThemeProvider>
         );
     }
 }
--- src/components/TextEntry/TextField.js
+++ src/components/TextEntry/TextField.js
@@ -1,20 +1,65 @@
 import React from "react";
 import Button from "@material-ui/core/Button/Button";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const styles = (theme) => ({
+    sendButton: {
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.primary.contrastText,
+        position: "absolute",
+        right: 50
+    },
+    textField: {
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.primary.contrastText
+    }
+});
 
 class TextField extends React.Component {
     state = {
         messageValue: ""
     };
 
+    onSend() {
+        if (this.state.messageValue === "") return;
+
+        console.log(this.state.messageValue);
+        this.setState({ messageValue: "" });
+    }
+
     render() {
+        const { classes } = this.props;
+
         return (
-            <div className={"TextField"} style={{ width: "100%", position: "absolute", bottom: 0, height: 250, border: "1px solid black" }}>
-                <input type="text" style={{ width: "50%", height: "100%" }} onChange={(e) => this.setState({ messageValue: e.target.value })} />
-                <Button
-                    onClick={() => {
-                        console.log(this.state.messageValue);
+            <div
+                className={"TextField"}
+                style={{
+                    width: this.props.width,
+                    position: "absolute",
+                    bottom: 0,
+                    height: 250,
+                    borderTop: "1px solid #292C33",
+                    padding: 20
+                }}
+            >
+                <input
+                    className={classes.textField}
+                    type="text"
+                    style={{
+                        position: "absolute",
+                        marginLeft: 0,
+                        marginBottom: 10,
+                        width: this.props.width * 0.75,
+                        height: "85%",
+                        outline: "none",
+                        border: "1px solid #292C33",
+                        padding: 10
                     }}
-                >
+                    value={this.state.messageValue}
+                    onKeyPress={(e) => (e.key === "Enter" ? this.onSend() : null)}
+                    onChange={(e) => this.setState({ messageValue: e.target.value })}
+                />
+                <Button disableRipple className={classes.sendButton} onClick={() => this.onSend()}>
                     Send
                 </Button>
             </div>
@@ -22,4 +67,4 @@ class TextField extends React.Component {
     }
 }
 
-export default TextField;
+export default withStyles(styles)(TextField);
