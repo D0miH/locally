5430531aae3df9fcf0ad87d3380c620b0f95fc31
Dominik <36657497+D0miH@users.noreply.github.com>
2018-10-31 16:41:45 +0100

    other user should now be discoverable

--- public/electron.js
+++ public/electron.js
@@ -9,6 +9,7 @@ const dgram = require("dgram");
 const ip = require("ip");
 
 let mainWindow;
+let pingIntervalHandle;
 
 function createWindow() {
     mainWindow = new BrowserWindow({ width: 900, height: 680, icon: path.join(__dirname, "assets/icons/png/64x64.png") });
@@ -24,21 +25,29 @@ app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
         app.quit();
     }
+
+    // close the sockets
+    messageSocket.close();
+    pingSocket.close();
 });
 // if the app is activated on macos create a new window
 app.on("activate", () => {
     if (mainWindow === null) {
         createWindow();
     }
+    messageSocket.bind(41234);
+    pingSocket.bind(41235);
+    clearInterval(pingIntervalHandle);
 });
 
-// create a new udp4 socket
-const socket = dgram.createSocket("udp4");
-// allow the socket to broadcast
-socket.on("listening", () => socket.setBroadcast(true));
+// create a new udp4 messageSocket for receiving
+const messageSocket = dgram.createSocket("udp4");
+// allow the messageSocket to broadcast
+// TODO: disable broadcast. This is not necessary for the messageSocket
+messageSocket.on("listening", () => messageSocket.setBroadcast(true));
 
 // callback when a message is received
-socket.on("message", (msg, msgInfo) => {
+messageSocket.on("message", (msg, msgInfo) => {
     if (msgInfo.address === ip.address()) {
         // if the source address is the own ip address return
         return;
@@ -47,12 +56,39 @@ socket.on("message", (msg, msgInfo) => {
         mainWindow.webContents.send("receivedMessage", msg.toString());
     }
 });
-
-// when the user wants to send a message send it using the socket
+// when the user wants to send a message send it using the messageSocket
 ipcMain.on("sendMessage", (event, message) => {
     let msg = Buffer.from(message);
-    socket.send(msg, 41234, "255.255.255.255");
+    messageSocket.send(msg, 41234, "255.255.255.255");
 });
-
 // listen on port 41234
-socket.bind(41234);
+messageSocket.bind(41234);
+
+// create a new socket to ping and discover new users
+const pingSocket = dgram.createSocket("udp4");
+// allow the pingSocket to broadcast
+pingSocket.on("listening", () => pingSocket.setBroadcast(true));
+// send a broadcast ping every 10 seconds
+listenForPings();
+// set the callback to receive the ping
+pingSocket.on("message", (msg, msgInfo) => {
+    console.log(ip.address().toString());
+    if (msgInfo.address === ip.address()) {
+        // if the source address is the own ip address return
+        return;
+    } else {
+        // check if the received message is "ping". When this is the case, send the source ip to the web application.
+        if (msg.toString() === "ping") {
+            mainWindow.webContents.send("newUser", ip.address().toString());
+        }
+    }
+});
+// listen on port 41235 to receive the ping
+pingSocket.bind(41235);
+
+function listenForPings() {
+    pingIntervalHandle = setInterval(() => {
+        let ping = Buffer.from("ping");
+        pingSocket.send(ping, 41235, "255.255.255.255");
+    }, 10000);
+}
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,38 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import ListItem from "@material-ui/core/ListItem/ListItem";
+import List from "@material-ui/core/List/List";
+const { ipcRenderer } = window.require("electron");
 
 class Sidebar extends React.Component {
+    state = {
+        userList: []
+    };
+
+    componentDidMount() {
+        // add a listener to the channel "newUser" of the ipc to receive messages
+        ipcRenderer.on("newUser", (event, message) => this.detectedNewUser(message));
+    }
+
+    detectedNewUser(ip) {
+        // add the new ip to the user list
+        let tmpList = this.state.userList;
+        tmpList.push(ip);
+        this.setState({ userList: tmpList });
+    }
+
+    getListItems() {
+        let list = [];
+        this.state.userList.forEach((ip, index) => {
+            list.push(<ListItem key={"listItem" + index} primary={ip} button />);
+        });
+    }
+
     render() {
         return (
             <div className={"Sidebar"} style={{ width: 250, height: "100%", position: "relative", float: "left", borderRight: "1px solid #292C33" }}>
                 <SearchBar />
+                <List>{this.getListItems()}</List>
             </div>
         );
     }
